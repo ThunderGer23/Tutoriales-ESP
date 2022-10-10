4af0f8456f049f776a20fd87f0212c3c6ed8b431
ThunderGer <Jaames11@hotmail.com>
2022-10-10 05:47:51 -0500

    Blink en app.py

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 from machine import Pin
 from time import sleep_ms as slms
 
+led = Pin(2, Pin.OUT)
+
+def main():
+    led.value(1)
+    slms(200)
+    led.value(0)
+    slms(200)
+    led.value(1)
+
+
+main()
